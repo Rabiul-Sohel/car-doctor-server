994f34f16f266e85fe326d517940dbd7359186aa
Rabiul Sohel <rabiulalamsohel@gmail.com>
2024-03-26 05:29:50 +0600

    fdfdf

---
+++ __deploy__.js
@@ -0,0 +1,5 @@
+/**
+ * server side deploy
+ * vercel config
+ * if you use cookies in cross site: use cors for your production url
+ */
--- index.js
+++ index.js
@@ -1,19 +1,53 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: [
+        // "http://localhost:5173",
+      "https://car-doctos.web.app/",
+      "https://car-doctos.firebaseapp.com/",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());  
+app.use(cookieParser());
 
 
-console.log(process.env.DB_PASS)
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const logger = (req, res, next) => {
+    console.log('log: info',req.method, req.url);
+    next()
+}
 
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token;
+    if (!token) {
+        return res.status(401).send({message: 'Not authorized'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+           return res.status(401).send({ message: "Not authorized" });
+        }
+        req.user = decoded;
+        next();
+    })
+    
+    // next()
+}
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ndy3clf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -22,16 +56,33 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
+  
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
         const serviceCollection = client.db('carDoctor').collection('services');
-        const bookingCollection = client.db('carDoctor').collection('bookings');
-
-        app.get('/services', async (req, res) => {
+        const bookingCollection = client.db("carDoctor").collection("orders");
+        // auth related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+              expiresIn: '1h',
+            });
+            res.cookie('token', token, {
+                    httpOnly: true,
+                    secure: false
+            }) 
+            .send({success: true})
+        })
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            console.log('logging out', user);
+            res.clearCookie('token', {maxAge: 0})
+            .send({success: true})
+        })
+        app.get('/services',  async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -52,19 +103,22 @@ async function run() {
 
 
         // bookings 
-        app.get('/bookings', async (req, res) => {
-            console.log(req.query.email);
+        app.get('/bookings', verifyToken, async (req, res) => {
+            
+            if (req.user.email !== req.query?.email) {
+                return res.status(403).send({message: 'Forbidden'})
+            }
             let query = {};
-            if (req.query?.email) {
-                query = { email: req.query.email }
+            if (req.query.email) {
+                query = { email: req.query?.email }
             }
             const result = await bookingCollection.find(query).toArray();
-            res.send(result);
+            res.send(result); 
         })
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
-            console.log(booking);
+            // console.log(booking);
             const result = await bookingCollection.insertOne(booking);
             res.send(result);
         });
